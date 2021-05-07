02fd74f04416d226adc7947d7d30580391cf2513
kawayiLinLin <1936635053@qq.com>
2021-05-07 17:18:08 +0800

    打印信息优化

--- server.js
+++ server.js
@@ -24,7 +24,17 @@ app.get('/lin', function(_req, res) {
 app.post('/deploy', function(_req, res) {
     console.log('start')
     const child_process = require('child_process')
-    child_process.execFile('./deploy.sh')
+    child_process.execFile('./deploy.sh', function(error, stdout,  stderr) {
+        if (error) {
+            console.log(error)
+        }
+        if (stdout) {
+            console.log(stdout)
+        }
+        if (stderr) {
+            console.log(stderr)
+        }
+    })
     console.log('end')
     res.end()
 })
