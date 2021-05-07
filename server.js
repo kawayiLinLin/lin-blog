1a33831363505a5e57aef7926acdd539f467894b
kawayiLinLin <1936635053@qq.com>
2021-05-07 16:23:28 +0800

    增加服务

---
+++ server.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json({ limit: '200mb' }))
+
+app.use('/public/', express.static('./public'))
+
+app.get('/', function(_req, res) {
+    res.redirect('/lin')
+})
+
+app.get('/lin', function(req, res) {
+    res.sendFile('./public/index.html')
+})
+
+
+const privateKey = fs.readFileSync('./2685862_yzl.xyz.key', 'utf8')
+
+const certificate = fs.readFileSync('./2685862_yzl.xyz.pem', 'utf8')
+
+const credentials = { key: privateKey, cert: certificate }
+
+const https = require('https').Server(credentials, app)
+
+https.listen(443, function() {
+  console.log('listening on *:2020')
+})
